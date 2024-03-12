4b0204fecc212448c5855155ad076a5d242e2a9d
Dkolomy <Dkolomy@hotmail.com>
2024-03-12 17:59:12 -0400

    status on 17:59

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const { ipcMain } = require("electron");
 const { app, BrowserWindow } = require("electron/main");
 const path = require("node:path");
 
@@ -10,16 +11,23 @@ function createWindow() {
     },
   });
 
+  ipcMain.on("set-title", (event, title) => {
+    const webContents = event.sender;
+    const win = BrowserWindow.fromWebContents(webContents);
+    win.setTitle(title);
+  });
+
+  //win.loadURL('https://dkolomy.com');
   win.loadFile("index.html");
 }
 
 app.whenReady().then(() => {
   createWindow();
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
